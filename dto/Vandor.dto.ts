3a074c8713eb1909424b7d55053229874d3ad373
ariunbold <ariuh0802@mail.com>
2021-06-24 23:28:10 +0300

    ehni odor

---
+++ dto/Vandor.dto.ts
@@ -0,0 +1,11 @@
+export interface CreateVandorInput{
+    name: string;
+    ownerName: string;
+    zaalType: [string];
+    price: string;
+    pincode: string;
+    address: string;
+    phone: string;
+    email: string;
+    password: string;
+}
